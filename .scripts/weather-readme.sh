62b39b5d290ead0c11df2655db3d0701a61c0e3b
Simón Villafañe <simonvillafanee@gmail.com>
2025-01-05 00:26:55 -0300

    fix(Root): tweek the weather unit to celsius

--- .scripts/weather-readme.sh
+++ .scripts/weather-readme.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/dash
 
-CELSIUS=$(curl -s https://es.wttr.in/Buenos%20Aires?format=1)
+CELSIUS=$(curl -s https://es.wttr.in/Buenos+Aires?format=1&m)
 
 sed -i "/<!--START_SECTION:weather-->/,/<!--END_SECTION:weather-->/c\\
 <!--START_SECTION:weather-->\\
