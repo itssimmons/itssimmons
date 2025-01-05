5fd4ac92558a8026de32cd3cc5ec080b436b5216
Simón Villafañe <simonvillafanee@gmail.com>
2025-01-04 23:48:45 -0300

    fix(Root): change format

--- .scripts/weather-readme.sh
+++ .scripts/weather-readme.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/dash
 
-RESPONSE=$(curl -s https://wttr.in?format=3 | sed 's/ //g' | cut -d ":" -f 2)
-CELSIUS=$(echo "$RESPONSE" | cut -c 2-)
+CELSIUS=$(curl -s https://wttr.in?format=1)
 
-sed -i "/<!--START_SECTION:weather-->/,/<!--END_SECTION:weather-->/c\
-<!--START_SECTION:weather-->\
-- 🌤 Working at <b>${CELSIUS}</b>\
-<!--END_SECTION:weather-->" README.md
+sed -i "/<!--START_SECTION:weather-->/,/<!--END_SECTION:weather-->/c\\
+<!--START_SECTION:weather-->\\
+- Working at <b>${CELSIUS}</b>\\
+<!--END_SECTION:weather-->\\
+" README.md
