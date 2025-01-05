25ff94ffd946937e37a0546a223bd87dfe51f177
Simón Villafañe <simonvillafanee@gmail.com>
2025-01-05 00:17:45 -0300

    fix(Root): tweeking a little bit

--- .scripts/weather-readme.sh
+++ .scripts/weather-readme.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/dash
 
-CELSIUS=$(curl -s https://wttr.in?format=1)
+CELSIUS=$(curl -s https://es.wttr.in/Buenos%20Aires?format=1)
 
 sed -i "/<!--START_SECTION:weather-->/,/<!--END_SECTION:weather-->/c\\
 <!--START_SECTION:weather-->\\
-- Working at <b>${CELSIUS}</b>\\
+Working at <b>${CELSIUS}</b>\\
 <!--END_SECTION:weather-->\\
 " README.md
