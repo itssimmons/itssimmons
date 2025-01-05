473333877d53759a18d8c179a379de6c02b9504c
Simón Villafañe <simonvillafanee@gmail.com>
2025-01-04 23:15:12 -0300

    Create weather-readme.sh

---
+++ .scripts/weather-readme.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+RESPONSE=$(curl -s https://wttr.in?format=3 | sed 's/ //g' | cut -d ":" -f 2)
+CELSIUS=${RESPONSE:1}
+
+sed -i '' "/<!--START_SECTION:weather-->/,/<!--END_SECTION:weather-->/c\\
+<!--START_SECTION:weather-->\\
+- 🌤 Working at <b>${CELSIUS}</b>\\
+<!--END_SECTION:weather-->\\
+" README.md
