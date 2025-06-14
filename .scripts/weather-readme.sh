b0d29433219fb83a89c4b25bf7f254471060d5ca
Simón Villafañe <simonvillafanee@gmail.com>
2025-06-13 21:02:51 -0300

    Update weather-readme.sh

--- .scripts/weather-readme.sh
+++ .scripts/weather-readme.sh
@@ -1,6 +1,10 @@
-#!/usr/bin/dash
+#!/usr/bin/bash
 
-CELSIUS=$(curl -s "https://es.wttr.in/Buenos+Aires?format=1&m")
+CELSIUS=$(curl -s "https://es.wttr.in/Buenos%20Aires?format=1&m")
+
+if [[ "$CELSIUS" == *"Unknown location"* ]]; then
+  CELSIUS="N/A"
+fi
 
 sed -i "/<!--START_SECTION:weather-->/,/<!--END_SECTION:weather-->/c\\
 \t\t<!--START_SECTION:weather-->\\
